15de001cd7860eccb47b0e51710aee081b0e5d4a
Tushar Jain <jain0735@gmail.com>
2024-04-06 01:54:18 +0530

    fix: broken changes

--- src/app.jsx
+++ src/app.jsx
@@ -1,6 +1,8 @@
 import * as React from 'react';
 import { createRoot } from 'react-dom/client';
 import Menu from './component/menu';
+
+
 const root = createRoot(document.body);
 root.render(<>
     <Menu />
--- src/component/menu.js
+++ src/component/menu.js
@@ -1,5 +1,4 @@
 import React, { useState } from 'react'
-import '..styles/window.css'
 
 const Menu= () => {
     const[menuOpen, setMenuOpen] = useState(false);
